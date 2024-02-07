20bd212244e6d0b46f31a71ca4e7e712e628eafe
Logan Cole <emtlogancole@gmail.com>
2024-02-07 16:15:22 -0600

    work on tests, add view_document.html file

--- reading_app/tests/test_views.py
+++ reading_app/tests/test_views.py
@@ -1,11 +1,13 @@
 from django.test import TestCase, Client
 from django.urls import reverse
-from reading_app.models import Document
+from reading_app.models import Document, Question
 from reading_app.forms import DocumentForm
 from django.core.files.uploadedfile import SimpleUploadedFile
 from unittest.mock import patch
 from reading_app.views import generate_questions
 import requests
+from django.shortcuts import get_object_or_404
+
 
 class TestUploadDocument(TestCase):
     def setUp(self):
@@ -51,4 +53,24 @@ class GenerateQuestionsTests(TestCase):
 
         # Assertions
         self.assertEqual(questions, [])
+class ViewDocumentTests(TestCase):
+    def setUp(self):
+        test_file = SimpleUploadedFile("test.txt", b"test content", content_type="text/plain")
+
+        self.document = Document.objects.create(title="Test Document", file=test_file)
+
+        self.url = reverse('view_document', args=(self.document.id,))
+
+    def test_view_document_with_existing_id(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'reading_app/view_document.html')
+        self.assertIn('document', response.context)
+        self.assertIn('questions', response.context)
+        self.assertEqual(response.context['document'], self.document)
+
+    def test_view_document_with_non_existing_id(self):
+        non_existing_id_url = reverse('view_document', args=(999,))
+        response = self.client.get(non_existing_id_url)
+        self.assertEqual(response.status_code, 404)
 
